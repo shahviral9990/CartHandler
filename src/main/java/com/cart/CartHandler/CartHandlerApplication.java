1e6493e5b6ff2d0fe72485a3b6ecbe89e50d2b98
Viral Shah <shahviral9990@gmail.com>
2018-01-13 21:02:03 +0530

    Initial commit

---
+++ src/main/java/com/cart/CartHandler/CartHandlerApplication.java
@@ -0,0 +1,12 @@
+package com.cart.CartHandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartHandlerApplication.class, args);
+	}
+}
